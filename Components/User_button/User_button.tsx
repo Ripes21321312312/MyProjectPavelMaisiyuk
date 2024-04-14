0c3e517042279fcc55f884795fc764da52567514
Паштет <102041061+Ripes21321312312@users.noreply.github.com>
2024-04-14 20:54:52 +0300

    Update User_button.tsx

--- Components/User_button/User_button.tsx
+++ Components/User_button/User_button.tsx
@@ -2,14 +2,12 @@
 import styles from "./User.module.css";
 
 const UserButton = ({ firstName, lastName }: { firstName: string, lastName: string }) => {
-    // Получаем первую букву имени и фамилии
     const firstInitial = firstName.charAt(0);
     const lastInitial = lastName.charAt(0);
 
     return (
         <button className={styles.UserButton}> {/* Используйте стили из объекта styles */}
             <div className={styles.icon}>
-                {/* Выводим иконку с первыми буквами имени и фамилии */}
                 {firstInitial}
                 {lastInitial}
             </div>
